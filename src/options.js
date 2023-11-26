65f8333cfc2e7dcb76d8b6c6442f7ce87f2103b7
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-11-26 13:09:25 +0100

    fix: show options pre-populated

--- src/options.js
+++ src/options.js
@@ -1,9 +1,23 @@
+// When the options page is loaded, populate the form with existing credentials
+document.addEventListener("DOMContentLoaded", loadCredentials);
+
+function loadCredentials() {
+  chrome.storage.sync.get(["username", "password"], (items) => {
+    if (items.username) {
+      document.getElementById("username").value = items.username;
+    }
+    if (items.password) {
+      document.getElementById("password").value = items.password;
+    }
+  });
+}
+
+// Save the credentials when the "Save" button is clicked
 document.getElementById("save").addEventListener("click", () => {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
   chrome.storage.sync.set({ username, password }, () => {
-    // Update the status to let the user know the credentials were saved.
     const status = document.getElementById("status");
     status.textContent = "Credentials saved successfully.";
     setTimeout(() => {
